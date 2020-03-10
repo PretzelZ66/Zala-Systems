2f241a282ca4890ff75166fe055ca134870e200a
Robyn <jadepretzel66@gmail.com>
2020-03-10 16:59:11 +1100

    0.0.0.1
    - Added the system block, which controls particular system variables.
    - Added the default password
    - Added the ability to choose a username

--- ZalaSystems.py
+++ ZalaSystems.py
@@ -1,4 +1,4 @@
-#0.0.0.0
+#0.0.0.1
 import time, random
 
 def pause(number):
@@ -10,3 +10,9 @@ def line(text):
     """Prints a line of text, then pauses the program for 1 second"""
     print(text)
     pause(1)
+
+
+#System Block
+password = zalasystems2020
+print("Please enter a name.")
+username = input(">>> ")
