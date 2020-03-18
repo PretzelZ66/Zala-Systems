e38258a928c15e2ff73b7534ea91f341200787c7
Robyn <jadepretzel66@gmail.com>
2020-03-18 15:28:14 +1100

    CF0.0.0.2
    - Added query, which asks the user for a question, then stores it in a variable

--- Core_Functions.py
+++ Core_Functions.py
@@ -1,4 +1,4 @@
-#0.0.0.1
+#0.0.0.2
 import time, random
 
 def pause(number):
@@ -16,3 +16,9 @@ def custom_line(text, number):
     """Prints a line of text, then pauses the program for a given number of time."""
     print(text)
     pause(number)
+
+
+def ask():
+    """Asks the user a question, then puts it into a variable."""
+    query = input(">>> ")
+    return  query
