da46470193df45171108f07f940d0c560a1b4fc5
Robyn <jadepretzel66@gmail.com>
2020-03-19 13:29:51 +1100

    ZS0.0.0.10
    - Added Whitespaces, to make the code more readable

--- ZalaSystems.py
+++ ZalaSystems.py
@@ -1,4 +1,4 @@
-#0.0.0.9
+#0.0.0.10
 import Core_Functions as cf
 
 
@@ -14,6 +14,7 @@ cf.line("Please enter a name.")
 username = cf.ask()
 cf.line(f"So your name is {username}?")
 confirm = cf.yn_ask()
+
 while confirm.lower() == "n":
     cf.line("Please enter a name.")
     username = cf.ask()
@@ -23,10 +24,12 @@ while confirm.lower() == "n":
 cf.line(f"Welcome, {username}!")
 cf.line("Please enter the current password")
 access = cf.ask()
+
 while access != password:
     cf.line(Error.one)
     cf.line("Please enter the current password")
     access = cf.ask()
+
 cf.line(f"Welcome, {username}, to Zala Systems.")
 cf.custom_line("LOADING...", 0.25)
 cf.line("Loaded")
@@ -35,36 +38,47 @@ while True:
     cf.line("Current menus: System")
     cf.line("Which menu would you like to open?")
     command = cf.ask()
+
     if command.lower() == "system":
         cf.line("Current system options: Change password, Exit")
         cf.line("Which option would you like to access?")
         option = cf.ask()
+
         if option.lower() == "system":
             cf.line("Would you like to change the password?")
             confirmation = cf.yn_ask()
+
             if confirmation.lower() == "y":
                 cf.line("Please enter the current password")
                 access = cf.ask()
+
                 while access != password:
                     cf.line(Error.one)
                     cf.line("Please enter the current password")
                     access = cf.ask()
+
                 cf.line("Please enter the new password")
                 password = cf.ask()
                 cf.line("Please confirm the new password")
                 confirm_password = cf.ask()
+
                 while confirm_password != password:
                     cf.line(Error.three)
                     cf.line("Please confirm the new password")
                     confirm_password = cf.ask()
+
                 cf.line("Password Updated!")
                 cf.line(f"The new password is {password}!")
+
         elif option.lower() == "exit":
             cf.line("Are you sure?")
             confirm = cf.yn_ask()
+
             if confirm.lower() == "y":
                 break
+
         else:
             cf.line(Error.two)
+
     else:
         cf.line(Error.two)
